309060a7b655a1fa8e857eb75e836524d72dfff4
Humoyun Mamasodiqov <mushtariy3748@gmail.com>
2025-10-31 21:11:29 +0500

    tez sot 0.28

--- beckend/urls.py
+++ beckend/urls.py
@@ -16,6 +16,5 @@ urlpatterns = [
     path('', include('fronend.urls')),  # asosiy app
     path('auth/', include('authentication.urls')),  # auth app
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
